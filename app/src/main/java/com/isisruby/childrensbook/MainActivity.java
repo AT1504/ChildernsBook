10b72480b549b7992d3b48de1075df75345e10c1
Ariel <arotev@gmail.com>
2017-03-03 23:06:07 +0200

    changes in the ui, made the spinner an Automatic editText completer also created the basis for the AboutActivity with fragments and added some res for buttons and stuf

--- app/src/main/java/com/isisruby/childrensbook/MainActivity.java
+++ app/src/main/java/com/isisruby/childrensbook/MainActivity.java
@@ -13,9 +13,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.AutoCompleteTextView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -25,51 +26,58 @@ import java.util.Locale;
 
 public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
     public EditText TaxYear;
-    public Spinner spinner;
+    public AutoCompleteTextView CitySpinner;
 
     private static final int RESULT_SETTINGS = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_new);
+        setContentView(R.layout.activity_main);
+        LinearLayout mLayout = (LinearLayout) findViewById(R.id.m_layout);
+        mLayout.requestFocus();
 
         TaxYear = (EditText) findViewById(R.id.tax_year_txt);
+        TaxYear.setFocusable(false);
+        TaxYear.setClickable(true);
         TaxYear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new DatePickerDialog(MainActivity.this, date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-            }
-        });
-
-        Button btnSettings=(Button)findViewById(R.id.buttonSettings);
+                                       @Override
+                                       public void onClick(View v) {
+                                           new DatePickerDialog(MainActivity.this, date, myCalendar
+                                                   .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                                                   myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                                       }
+                                   }
+        );
+        //Button btnSettings=(Button)findViewById(R.id.buttonSettings);
 
         // start the UserSettingActivity when user clicks on Button
-        btnSettings.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), UserSettingActivity.class);
-                startActivityForResult(i, RESULT_SETTINGS);
-            }
-        });
+//        btnSettings.setOnClickListener(new View.OnClickListener() {
+//
+//            public void onClick(View v) {
+//                Intent i = new Intent(getApplicationContext(), UserSettingActivity.class);
+//                startActivityForResult(i, RESULT_SETTINGS);
+//            }
+//        });
         displayUserSettings();
         CityChooserBySpinner();
     }
 
-    public void CityChooserBySpinner(){
-        spinner = (Spinner) findViewById(R.id.citys_spinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.citys_array, android.R.layout.simple_spinner_item);
+    public void CityChooserBySpinner() {
+        CitySpinner = (AutoCompleteTextView) findViewById(R.id.citys_spinner);
+        // Create an ArrayAdapter using the string array and a default CitySpinner layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, CITIES);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-        spinner.setBackgroundColor(getResources().getColor(R.color.accent_material_dark));
-        spinner.setOnItemSelectedListener(this);
+        // Apply the adapter to the CitySpinner
+        CitySpinner.setAdapter(adapter);
+        CitySpinner.setOnItemSelectedListener(this);
+        CitySpinner.setSingleLine();
     }
+    private static final String[] CITIES = new String[] {
+            "אשדות יעקב", "ariel", "יונתן", "עזוז", "פקיעין"
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -80,15 +88,23 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Intent i;
+
         switch (item.getItemId()) {
 
             case R.id.action_settings:
-                Intent i = new Intent(this, UserSettingActivity.class);
-                startActivityForResult(i, RESULT_SETTINGS);
+                i = new Intent(this, UserSettingActivity.class);
+                //startActivityForResult(i, RESULT_SETTINGS);
+                startActivity(i);
+                break;
+            case R.id.action_about:
+                i = new Intent(this, AboutActivity.class);
+                startActivity(i);
                 break;
         }
         return true;
     }
+
     Calendar myCalendar = Calendar.getInstance();
 
     DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
@@ -103,6 +119,7 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
         }
 
     };
+
     private void updateLabel() {
 
         String myFormat = "dd/MM/yy";
@@ -112,75 +129,40 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == RESULT_SETTINGS)
-        {
+        if (requestCode == RESULT_SETTINGS) {
             displayUserSettings();
         }
 
     }
 
-    private void displayUserSettings()
-    {
+    private void displayUserSettings() {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-        String  settings = "";
-
-        settings=settings+"Password: " + sharedPrefs.getString("prefUserPassword", "NOPASSWORD");
-
-        settings=settings+"\nRemind For Update:"+ sharedPrefs.getBoolean("prefLockScreen", false);
-
-        settings=settings+"\nUpdate Frequency: "
+        String settings = "";
+        settings = settings + "Password: " + sharedPrefs.getString("prefUserPassword", "NOPASSWORD");
+        settings = settings + "\nRemind For Update:" + sharedPrefs.getBoolean("prefLockScreen", false);
+        settings = settings + "\nUpdate Frequency: "
                 + sharedPrefs.getString("prefUpdateFrequency", "NOUPDATE");
-
         TextView textViewSetting = (TextView) findViewById(R.id.textUserSettings);
-
         textViewSetting.setText(settings);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // implementation for the spinner
+        // implementation for the CitySpinner
         // Spinner view value
         String city = parent.getItemAtPosition(position).toString();
-        Log.i("city",city);
-        String text = spinner.getSelectedItem().toString();
-        Log.i("text",text);
+        Log.i("city", city);
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-        // another implementation for the spinner
+        // another implementation for the CitySpinner
     }
 }
-/*public class MainActivity extends AppCompatActivity implements Button.OnClickListener{
-    //FragmentPagerAdapter adapterViewPager;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_new);
 
-        //ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
-        //adapterViewPager = new CustomPagerAdapter(getSupportFragmentManager());
-        //vpPager.setAdapter(adapterViewPager);
-
-        Button b = (Button) findViewById(R.id.button);
-        b.setOnClickListener(this);
-
-        }
-
-    @Override
-    public void onClick(View v) {
-
-        if (v.getId() == R.id.button){
-            Intent intent = new Intent(this, TofesFragment.class);
-            startActivity(intent);
-        }
-    }
-}*/
 
 
 
